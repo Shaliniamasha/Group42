6c82c2755ca19f529bfcdd7b56a4f0c895a86710
SDilhara19 <sdilharabandara@gmail.com>
2023-09-27 22:49:41 +0530

    changing the folder strucure

---
+++ index.php
@@ -0,0 +1,7 @@
+ <?php include('includes/header.php'); ?>
+    <main>
+        <div class="home-image-container">
+            <img src="G:\SDB\Year 2 -Semester 1\SCS2202 Group Project I\Give4Good\Group42\Assets\homepageimage.png" alt="image">
+        </div>
+    </main>
+<?php include('includes/footer.php'); ?>
