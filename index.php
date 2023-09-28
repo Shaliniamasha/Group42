1b24133a242fb1ac1dc4be7f2b3560a738d3d6c7
SDilhara19 <sdilharabandara@gmail.com>
2023-09-28 22:59:00 +0530

    adding connection.php

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
- <?php include('includes/header.php'); ?>
+ <?php 
+ require_once('includes/connection.php');
+ include('includes/header.php'); ?>
     <main>
         <div class="home-image-container">
             <img src=" " alt="image">
             <h1>Home Page</h1>
         </div>
     </main>
-<?php include('includes/footer.php'); ?>
+<?php 
+include('includes/footer.php'); 
+mysqli_close($connection); 
+?>
