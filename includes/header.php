66084875dcbe067710962b975777790bb2e40ab3
SDilhara19 <sdilharabandara@gmail.com>
2023-10-02 06:21:40 +0530

    Adding dummy images for fundraisers and changing the header

--- includes/header.php
+++ includes/header.php
@@ -8,33 +8,43 @@
 </head>
 <body>
 <header>
-        <div class="navbar1">
-            <nav>
-                <ul class="nav_links">
-                    <li><a href="postPage.php">Post Page</a></li>
-                    <li><a href="org.php">Organisations</a></li>
-                </ul>
-            </nav>
-            <img class="logo" src="Assets/icons/logo.svg" alt="logo" height="50px">
-            <nav>
-                <ul class="nav_links">
-                    <li><a href="about.php">About</a></li>
-                    <li><a href="startFundraiser.php">Start a Fundraiser</a></li>
-                </ul>
-            </nav>
+<header>
+        <div class="nav">
+            <div class="navbar1">
+                <nav>
+                    <ul class="nav_links">
+                        <li><a href="postPage.php">Post Page</a></li>
+                        <li><a href="org.php">Organisations</a></li>
+                    </ul>
+                </nav>
+                <img class="logo" src="Assets/icons/logo.svg" alt="logo" height="50px">
+                <nav>
+                    <ul class="nav_links">
+                        <li><a href="about.php">About</a></li>
+                        <li><a href="startFundraiser.php">Start a Fundraiser</a></li>
+                    </ul>
+                </nav>
 
+            </div>
+            <div class="navbar2">
+                <ul class="nav_links">
+                    <li><a href="#"><button>Dashboard</button></a></li>
+                </ul>
+                <form action="" class="search-bar">
+                    <input type="text" placeholder="Search give4Good" name="search">
+                    <button type="submit"><img src="Assets/icons/search.svg"></button>
+                </form>
+                <ul class="nav_links">
+                    <li><a href="#"><button>Sign Up</button></a></li>
+                    <li><a href="#"><button>Login</button></a></li>
+                </ul>
+            </div>
         </div>
-        <div class="navbar2">
-            <ul class="nav_links">
-                <li><a href="#"><button>Dashboard</button></a></li>
-            </ul>
-            <form action="" class="search-bar">
-                <input type="text" placeholder="Search give4Good" name="search">
-                <button type="submit"><img src="Assets/icons/search.svg"></button>
-            </form>
-            <ul class="nav_links">
-                <li><a href="#"><button>Sign Up</button></a></li>
-                <li><a href="#"><button>Login</button></a></li>
-            </ul>
+        <div class="home-image-container">
+            <div class="homepage-image-left"></div>
+            <div class="homepage-image-right">
+                <h1>Home Page</h1>
+            </div>
         </div>
+    </header>
     </header>
