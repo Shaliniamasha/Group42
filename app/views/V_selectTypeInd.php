f69e7529ea9fdf40a5a4b43f556f3f8b4677dbe4
Janith Kaluarachchi <janith.thenuja@gmail.com>
2023-11-01 23:05:44 +0530

    Fundr type correction

--- app/views/V_selectTypeInd.php
+++ app/views/V_selectTypeInd.php
@@ -5,21 +5,21 @@
 <main>
     <h1>Choose type of Fundraiser:</h1>
     <div class="option-container">
-        <a href="<?php echo URLROOT ?>/StartFund/Common">
+        <a href="<?php echo URLROOT ?>/StartFund/Houseclass">
             <img class= "img1" src="../public/Assets/images/Housing.jpg" alt="Housing">
             <p>Fundraiser for Housing</p>
         </a>
 
-        <a href="<?php echo URLROOT ?>/StartFund/Common">
+        <a href="<?php echo URLROOT ?>/StartFund/Healthclass">
             <img class= "img1" src="../public/Assets/images/Health.jpg" alt="Health">
             <p>Fundraiser for Medical Reasons</p>
         </a>
 
-        <a href="<?php echo URLROOT ?>/StartFund/Common">
+        <a href="<?php echo URLROOT ?>/StartFund/Educationclass">
             <img class= "img1" src="../public/Assets/images/Education.jpg" alt="Education">
             <p>Fundraiser for Educational purpose</p>
         </a>
-        <a href="<?php echo URLROOT ?>/StartFund/Common">
+        <a href="<?php echo URLROOT ?>/StartFund/Sportclass">
             <img class= "img1" src="../public/Assets/images/Sports.jpg" alt="Sports">
             <p>Fundraiser for Sports</p>
         </a>
